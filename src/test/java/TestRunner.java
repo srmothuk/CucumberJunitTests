2d62c161d64cee362f9aaa20411d852a1960f20e
msreeni <40213727+srmothuk@users.noreply.github.com>
2018-06-30 18:01:49 +0530

    Uploaded CucumberJunitTests project

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,14 @@
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/test/resources/features"
+		,glue={"stepdefinitions"}
+		)
+
+public class TestRunner {
+
+}
---
+++ src/test/java/stepdefinitions/login.java
@@ -0,0 +1,27 @@
+package stepdefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+
+public class login {
+	
+	@Given("^I want to write a step with precondition$")
+	public void i_want_to_write_a_step_with_precondition() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+//	    throw new PendingException();
+	}
+
+	@When("^I complete action$")
+	public void i_complete_action() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+//	    throw new PendingException();
+	}
+
+	@Then("^I validate the outcomes$")
+	public void i_validate_the_outcomes() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+//	    throw new PendingException();
+	}
+
+}
